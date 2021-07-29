71ebca4a31bf6cd21b2efa391964cfb33e0c88d1
Greyson Newton <greynewt@tamu.edu>
2021-07-29 12:26:48 -0800

    Condensed Project

--- expDecayTest.py
+++
@@ -1,6 +0,0 @@
-from expDecay import *
-xinit, xfinal = 0, 10
-expFit = expDecay(xinit, xfinal, 1.67)
-stepSize = expFit.returnStepSize(0)
-print(stepSize)
-#expFit.plot()
--- setup.py
+++
@@ -1,3 +0,0 @@
-from distutils.core import setup
-import py2exes 
-setup(console=['GUI.py'], zipfile = None)
